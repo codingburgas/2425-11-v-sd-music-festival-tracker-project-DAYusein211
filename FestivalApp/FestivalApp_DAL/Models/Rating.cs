cd969aecd350434aff93e3c3981fce77b7f7801b
DAYusein211 <DAYusein21@codingburgas.bg>
2025-01-25 15:18:31 +0200

    Fix minor issue

--- FestivalApp/FestivalApp_DAL/Models/Rating.cs
+++ FestivalApp/FestivalApp_DAL/Models/Rating.cs
@@ -5,13 +5,10 @@ namespace FestivalApp_DAL.Models
 {
     public class Rating
     {
-        [Key] 
         public int Id { get; set; }
-
-        [ForeignKey("Guest")] 
+        
         public int GuestId { get; set; }  
         
-        [ForeignKey("Artist")]
         public int ArtistId { get; set; } 
         
         public float RatingValue { get; set; } 
